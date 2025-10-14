0707b0f973b1954da694a9dd3aaf48acc9959004
rapite <andy02lee1@gmail.com>
2025-10-14 18:13:16 -0400

    delete a book

--- backend/index.js
+++ backend/index.js
@@ -100,6 +100,23 @@ app.put('/books/:id', async (request, response) => {
     }
 })
 
+app.delete('/books/:id', async (request, response) => {
+    try {
+        const { id } = request.params;
+        
+        const result = await Book.findByIdAndDelete(id);
+
+        if (!result) {
+            return response.status(404).json({ message: 'Book not found'})
+        }
+
+        return response.status(200).send({ message: 'Book deleted successfully' })
+    } catch (error) {
+       console.log(error.message) 
+       response.status(500).send({ message: error.mesage})
+    }
+})
+
 mongoose
     .connect(mongoDBURL)
     .then(()=> {
