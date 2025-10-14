87d98672dd55e49e865fb2e8ef8830edf4b598b9
rapite <andy02lee1@gmail.com>
2025-10-14 19:10:19 -0400

    finished backend

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,23 @@ import { PORT, mongoDBURL } from  "./config.js"
 import mongoose from 'mongoose'
 import { Book } from "./models/bookModel.js";
 import booksRoute from './routes/booksRoute.js'
+import cors from 'cors'
 
 const app = express()
 
 // Middleware for parsing request body
 app.use(express.json());
+// Option 1: Allow all origins with default of cors
+
+//app.use(cors())
+// Option 2: allow custon origins
+app.use(
+    cors({
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type'],
+    })
+)
 
 app.get('/', (request, response) => {
     console.log(request)
